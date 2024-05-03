5991fb27f7d618c6f4a22fef2c30c9f2d8a72461
hemanth-sunkireddy <hemanthkumarreddysunkireddy51@gmail.com>
2024-05-03 14:08:59 +0530

    testing response from CAS

---
+++ index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const Login = require("./routes/login");
+var session = require("express-session");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(
+  session({
+    secret: "qwertyuhjjdfghjkl",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use("/login", Login);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
---
+++ routes/login.js
@@ -0,0 +1,84 @@
+const CASAuthentication = require("node-cas-authentication");
+const router = require("express").Router();
+
+const jwt = require("jsonwebtoken");
+
+
+var cas = new CASAuthentication({
+  cas_url: "https://login.iiit.ac.in/cas",
+  service_url: "http://localhost:5000/login",
+  cas_version: "3.0",
+  renew: false,
+  is_dev_mode: false,
+  dev_mode_user: "",
+  dev_mode_info: {},
+  session_name: "cas_user",
+  session_info: "cas_userinfo",
+  destroy_session: false,
+  return_to: "https://google.com",
+});
+
+router.get("/", cas.bounce, async (req, res) => {
+  const ticket = req.query.ticket;
+
+  if (ticket) {
+    cas.validate(ticket, function (err, status, username, extended) {
+      if (err) {
+        res.status(500);
+        res.send(err);
+        return;
+      }
+
+      if (status) {
+        res.send(
+          "Success! Loggeeed in as " +
+            username +
+            " with extended attributes: " +
+            JSON.stringify(extended)
+        );
+      }
+    });
+  }
+
+  const user = req.session[cas.session_info || {}];
+  const email = user["e-mail"];
+  const name = user["name"];
+
+  const payload = {
+    email: email,
+    name: name,
+  };
+
+  const token = jwt.sign(payload, "qwertyuhjjdfghjkl");
+
+  res.cookie("token", token, { httpOnly: true });
+  res.cookie("name", name, { httpOnly: false });
+  res.cookie("email", email, { httpOnly: false });
+  res.redirect(process.env.REDIRECT_URL);
+});
+
+router.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.clearCookie("name");
+  res.clearCookie("email");
+  res.redirect("https://login.iiit.ac.in/cas" + "/logout");
+});
+
+router.get("/validate", (req, res) => {
+  const token = req.cookies.token;
+  const name = req.cookies.name;
+  const email = req.cookies.email;
+
+  if (!token || !name || !email) {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
+  }
+
+  res.json({
+    token: token,
+    name: name,
+    email: email,
+  });
+});
+
+module.exports = router;
